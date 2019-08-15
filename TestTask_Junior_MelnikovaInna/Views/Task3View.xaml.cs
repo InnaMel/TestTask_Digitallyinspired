7c27a3ded4281b23f59d26a9e909d45d100def35
Inna <Inna@DESKTOP-DDRRGAF>
2019-08-15 06:40:34 +0300

    Added Objects for project

--- TestTask_Junior_MelnikovaInna/Views/Task3View.xaml.cs
+++ TestTask_Junior_MelnikovaInna/Views/Task3View.xaml.cs
@@ -3,6 +3,7 @@ using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
